8168ba81aa6542696ccdb760aa30a8232ee6a2c6
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-13 11:55:53 +0100

    Random avatar example modified and moved to examples

--- example.py
+++
@@ -1,5 +0,0 @@
-from python_avataaars import Avatar
-
-# Generate 3 random avatars
-for i in range(3):
-    Avatar.random().render('avatar_{}.svg'.format(i))
---
+++ examples/random/__main__.py
@@ -0,0 +1,10 @@
+import random
+
+from python_avataaars import Avatar
+
+# Set a seed
+random.seed(123)
+
+# Generate 10 random avatars
+for i in range(10):
+    Avatar.random().render('avatar_{}.svg'.format(i))
