f3444a034161b42ecc1c6f7ed96e42b8e007f64f
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-13 19:26:27 +0100

    core documented and code redundancy removed

--- python_avataaars/core.py
+++ python_avataaars/core.py
@@ -9,16 +9,43 @@ _package_path = os.path.dirname(__file__)
 _default_path = os.path.join(_package_path, 'default.json')
 _installed_path = os.path.join(_package_path, 'installed.json')
 
-def install_part(part_path, part_type, print_messages=False):
+def _check(condition, message):
+    """
+    Check wether a condition holds or not. If the condition is false, raises a RuntimeError with the provided message
 
-    if not part_type.__install__:
-        raise RuntimeError("The part type {} cannot be expanded".format(part_type.__name__))
+    :param condition: The condition to test
+    :param message: The message to show if the condition is false
 
-    if part_type.__path__ == '':
-        raise RuntimeError("Installation path not found")
+    :type condition: bool
+    :type message: str
 
-    file_name = os.path.splitext(os.path.basename(part_path))[0]
-    const_name = _sanitize(file_name) 
+    :raises RuntimeError: The error is raised if the condition in ``condition`` is false
+    """
+    if condition:
+        raise RuntimeError(message)
+
+def _install_enum(name, value, part_type, enum_type):
+    """
+    Adds a value to a enum and returns the enum with the new value
+
+    :param name: The name of the value to add
+    :param value: The value to add
+    :param part_type: The enum to extend
+    :param enum_type: The type of the enum to extend
+
+    :type name: str
+    :type value: str
+    :type part_type: :class:`AvatarEnum`
+    :type enum_type: :class:`Avatarpart` or :class:`AvatarColor`
+
+    :raises RuntimeError: The error is raised if the enum cannot be expanded
+    :raises RuntimeError: Raised when the enum contains an item named ``name``
+
+    :return: :class:`AvatarEnum` with the new value added
+    """
+    _check(not part_type.__install__, "{} cannot be expanded".format(part_type.__name__))
+    
+    const_name = _sanitize(name) 
     
     # Load defaults
     with open(_default_path, 'r') as f:
@@ -40,28 +67,48 @@ def install_part(part_path, part_type, print_messages=False):
         installed_values[part_type.__name__] = {}
 
     # Check wether the value exists or not
-    if const_name in combined:
-        raise RuntimeError("The part {}.{} already exists".format(part_type.__name__, const_name))
+    _check(const_name in combined, "{}.{} already exists".format(part_type.__name__, const_name))
 
     # Add to installed
-    installed_values[part_type.__name__][const_name] = file_name
+    installed_values[part_type.__name__][const_name] = value
 
     # Save installed
     with open(_installed_path, 'w') as f:
         json.dump(installed_values, f, indent=4)
 
-    # Copy the file
-    destination = _get_path(part_type, file_name)
-    shutil.copy(part_path, destination)
-
     # Overwrite the enum file
     combined = {**default_values[part_type.__name__], **installed_values[part_type.__name__]}
-    _write_enum(part_type, combined, AvatarPart)
+    _write_enum(part_type, combined, enum_type)
 
-    if print_messages:
-        print('"{}" installed at "{}" as {}.{}. Reload the library to use it'.format(part_path, destination, part_type.__name__, const_name))
+    return True
 
-def uninstall_part(part, confirm=True):
+def _prompt_confirmation(class_name, enum_name):
+    """
+    Prompt a deletion confirmation message. Returns ``True`` when the user accepted and ``False`` when declined
+
+    :param class_name: The name of the enum from which the value will be deleted
+    :param enum_name: The name of the value to delete
+
+    :type class_name: str
+    :type enum_name: str
+
+    :return: bool
+    """
+    confirm_resp = input('Do you really want to uninstall {}.{}? y/n: '.format(class_name, enum_name))
+    return confirm_resp.lower().strip() == 'y'
+
+def _uninstall_enum(part, enum_type):
+    """
+    Removes a value for a given enum. Returns ``True`` on sucess, ``False`` otherwise
+
+    :param part: The name of the value to remove
+    :param enum_type: The type of the enum from which the value will be removed
+
+    :type part: :class:`AvatarEnum`
+    :type enum_type: :class:`AvatarPart` or :class:`AvatarColor`
+
+    :return: bool
+    """
     if os.path.isfile(_installed_path):
         # Load installed
         with open(_installed_path, 'r') as f:
@@ -70,15 +117,6 @@ def uninstall_part(part, confirm=True):
         # Load default
         with open(_default_path, 'r') as f:
             default_values = json.load(f)
-
-        
-        if confirm:
-            confirm_resp = input('Do you really want to uninstall {}.{}? y/n: '.format(part.__class__.__name__, part.name))
-            if confirm_resp.lower().strip() != 'y':
-                return False
-
-        # Delete svg
-        os.remove(_get_path(part.__class__, part.value))
 
         # Delete from installed
         del installed_values[part.__class__.__name__][part.name]
@@ -91,28 +129,72 @@ def uninstall_part(part, confirm=True):
             json.dump(installed_values, f)
 
         # Rewrite enum
-        _write_enum(part.__class__, combined, AvatarPart)
+        _write_enum(part.__class__, combined, enum_type)
         return True
+    return False
 
-def install_color(name, value, print_messages=False):
-    raise NotImplementedError("TODO")   # TODO
-    # Load installed.json
-    # Load default.json
-    # Combine installed + default
-    # Check conflicts
-    # Check installable
-    # Add value to installed
-    # Combine installed + default
-    # Rewrite enum
+def install_part(part_path, part_type):
+    """
+    Installs a part (clothing, hair type, eye type...). Returns the enum with the new installed value added
+
+    :param part_path: The path to the svg to install
+    :param part_type: The enum where the new value will be installed
+
+    :type part_path: str
+    :type part_type: :class:`AvatarPart`
+
+    :raise RuntimeError: Whenever the part_type does not have an installation path (in other words, when it is not an AvatarPart or when it is not installable)
+
+    :return: :class:`AvatarPart`
+    """
+    _check(part_type.__path__ == '', "Installation path not found")
+    file_name = os.path.splitext(os.path.basename(part_path))[0]
+
+    new_enum = _install_enum(file_name, file_name, part_type, AvatarPart)
+
+    if new_enum:
+
+        # Copy the file on success
+        destination = _get_path(part_type, file_name)
+        shutil.copy(part_path, destination)
+
+    # Return the enum with the new value
+    return new_enum
+
+def uninstall_part(part, confirm=True):
+    """
+    Removes an installed part. Returns ``True`` on success, ``False`` otherwise
+
+    :param part: The part to uninstall
+    :param confirm: Does this action require confirmation? If ``True``, the user will be asked to confirm the uninstall
+
+    :type part: :class:`AvatarPart`
+    :type confirm: bool
+
+    :return: bool
+    """
+    if _prompt_confirmation(part.__class__.__name__, part.name) and _uninstall_enum(part, AvatarPart):
+        os.remove(_get_path(part.__class__, part.value))
+        return True
+    return False
+
+def install_color(name, value, part_type):
+    _check(part_type.__path__ != '', "{} is not a color".format(part_type.__name__))
+    return _install_enum(name, value, part_type, AvatarColor)
 
 def uninstall_color(color, confirm=True):
-    raise NotImplementedError("TODO")   # TODO
-    # Load installed.json
-    # Delete from installed
-    # Combine installed + default
-    # Rewrite enum
+    if confirm:
+        return _prompt_confirmation(color.__class__.__name__, color.name) and _uninstall_enum(color, AvatarColor)
+    return _uninstall_enum(color, AvatarColor)
+
 
 def factory_reset(confirm=True):
+    """
+    Resets the package to its original state
+
+    :param confirm: Does this action require confirmation? If ``True``, the user will be asked to confirm the reset
+    :type confirm: bool
+    """
     raise NotImplementedError("TODO")   # TODO
 
     # Load installed and remove svgs
@@ -133,16 +215,43 @@ def factory_reset(confirm=True):
         _write_enum()
 
 def _get_path(enum_cls, value):
+    """
+    Returns the path to the svg file for an AvatarPart given the enum and the name
+
+    :param enum_cls: The enum
+    :param value: The name
+
+    :return: str
+    """
     p = os.path.join(_package_path, enum_cls.__path__, '{}.svg'.format(value))
     return p
 
 def _sanitize(value):
+    """
+    Prepares a string to be used as a Python identifier
+
+    :param value: The string to convert to a valid Python identifier
+
+    :type value: str
+    
+    :return: str
+    """
     value = re.sub('[^A-Za-z0-9_]+', '_', value)    # Remove invalid Python identifier chars
     value = value.lstrip('0123456789')              # Remove digits fro the beginning
     return value.upper()                            # Return uppercase
 
 def _write_enum(e, values_dict, t):
+    """
+    Writes an enum to an importable file
 
+    :param e: The original enum to overwrite
+    :param values_dict: Dictionary holding the values for the new enum
+    :param t: Type of the enum
+
+    :type e: :class:`AvatarEnum`
+    :type values_dict: dict
+    :type t: :class:`AvatarPart` or :class:`AvatarColor`
+    """
     filename = e.__enum_path__
     name = e.__name__
     path = e.__path__
