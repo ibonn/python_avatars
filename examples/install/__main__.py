a085da0aef04b655f0c85ee82bb5b9b5e53f2313
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-13 19:14:25 +0100

    install example modified

--- examples/install/__main__.py
+++ examples/install/__main__.py
@@ -1,12 +1,6 @@
-from python_avataaars import install_part, ClothingType, Avatar
+import python_avataaars as pa
 
-# You can reload the library or assign the value returned by install_part to
-# the enum to use the new value
-ClothingType = install_part(
-    'suit.svg',
-    ClothingType,
-    print_messages=True
-)
+# IMPORTANT: You should run install.py before running this script, otherwise it won't work.
 
 # Generate a random avatar wearing the new installed clothes
-Avatar.random(clothing=ClothingType.SUIT).render("avatar_suit.svg")
+pa.Avatar.random(clothing=pa.ClothingType.SUIT).render("avatar_suit.svg")
---
+++ examples/install/install.py
@@ -0,0 +1,4 @@
+import python_avataaars as pa
+
+# Install the new part
+pa.install_part('suit.svg', pa.ClothingType)
