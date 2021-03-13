6db78f8f5f512446a926b58d8739513ad937b05e
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-13 19:14:05 +0100

    uninstall example added

---
+++ examples/uninstall/__main__.py
@@ -0,0 +1,6 @@
+import python_avataaars as pa
+
+# IMPORTANT: You should run the example at examples/install before running this script
+
+# Uninstall the part installed with install example
+pa.uninstall_part(pa.ClothingType.SUIT)
