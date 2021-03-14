c5e6c41619f9eb99ec24881a4f5da820d0e1a6bd
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-14 13:17:02 +0100

    avatar generation example updated

--- examples/avatar/__main__.py
+++ examples/avatar/__main__.py
@@ -1,18 +1,18 @@
-from python_avataaars import *
+import python_avataaars as pa
 
-Avatar(
-    style=AvatarStyle.CIRCLE,
-    top=HairType.STRAIGHT_2,
-    eyebrows=EyebrowType.DEFAULT_NATURAL,
-    eyes=EyeType.DEFAULT,
-    nose=NoseType.DEFAULT,
-    mouth=MouthType.EATING,
-    facial_hair=FacialHairType.NONE,
+pa.Avatar(
+    style=pa.AvatarStyle.CIRCLE,
+    top=pa.HairType.STRAIGHT_2,
+    eyebrows=pa.EyebrowType.DEFAULT_NATURAL,
+    eyes=pa.EyeType.DEFAULT,
+    nose=pa.NoseType.DEFAULT,
+    mouth=pa.MouthType.EATING,
+    facial_hair=pa.FacialHairType.NONE,
     # You can use hex colors on any color attribute...
     skin_color='#00FFFF',
     # Or you can use the colors provided by the library
-    hair_color=HairColor.BLACK,
-    accessory=AccessoryType.NONE,
-    clothing=ClothingType.HOODIE,
-    clothing_color=ClothingColor.HEATHER
+    hair_color=pa.HairColor.BLACK,
+    accessory=pa.AccessoryType.NONE,
+    clothing=pa.ClothingType.HOODIE,
+    clothing_color=pa.ClothingColor.HEATHER
 ).render("my_avatar.svg")
