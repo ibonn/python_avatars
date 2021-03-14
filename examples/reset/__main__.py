c9884b22d711dcb62ca4138901f07e5dab66263c
Ibon <37736154+ibonn@users.noreply.github.com>
2021-03-14 12:09:50 +0100

    factory_reset() example added

---
+++ examples/reset/__main__.py
@@ -0,0 +1,7 @@
+from python_avataaars import factory_reset
+
+# WARNING: Running this script will reset the library to its default state.
+#          This means all the colors and parts (eyes, eyebrows, nose types,
+#          clothing types...) you installed will be removed.
+
+factory_reset()
