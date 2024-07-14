23163416b4f8072a9b3fe1d99dca67cdbb719e6e
Ibon <ibonescartin@gmail.com>
2024-07-14 11:33:46 +0200

    Bump version

--- python_avatars/__init__.py
+++ python_avatars/__init__.py
@@ -4,7 +4,7 @@ SVG avatar generation library
 More info @ https://github.com/ibonn/python_avatars
 """
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 
 # Enums
 from .accessory_types import AccessoryType
